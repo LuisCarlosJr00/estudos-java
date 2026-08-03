9d50b9e590c999c7fec21ffa6baf18405702d5f9
Luis Carlos Ferreira Junior <160869261+LuisCarlosJr00@users.noreply.github.com>
2026-08-03 17:59:38 -0300

    Add files via upload

---
+++ iniciante/estruturas-condicionais/If_Else.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class If_Else {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner sc = new Scanner(System.in);
+		
+		double salario = sc.nextDouble(); 
+		double imposto;
+		
+		
+		
+		if ( salario <= 2000.0 ) {
+			imposto = 0.0;
+		}
+		else if ( salario <= 3000.0){ 
+			imposto = (salario - 2000.0 * 0.08);
+		}
+		else if (salario <= 4500.0) {
+			imposto = (salario - 3000.0) * 0.18 + 1000.0 * 0.8;
+		}
+		else {
+			imposto = (salario - 4500.0) * 0.28 + 1500.0 * 0.18  + 1000.0 * 0.8;
+		}
+
+		if (imposto == 0.0) {
+			System.out.println("INSENTO");
+		}
+		else {
+			System.out.printf(" %2.f \n", imposto);
+		}
+		sc.close();
+	}
+
+}
