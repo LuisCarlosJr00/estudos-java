4bac798d37e3591602a1a824b4d3deb1827bf2fb
Luis Carlos Ferreira Junior <160869261+LuisCarlosJr00@users.noreply.github.com>
2026-08-03 18:01:20 -0300

    Add files via upload

---
+++ iniciante/matrizes/program2.java
@@ -0,0 +1,58 @@
+package Aplication;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class program2 {
+
+	public static void main(String[] args) {
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		int m,n;
+		System.out.println("Leia o número de linhas e colunas da Matriz, respectivamente: ");
+		m = sc.nextInt();
+		n = sc.nextInt();
+		
+		int [][] matriz = new int[m][n];
+		System.out.println("Digite a Matriz: ");
+		for (int i=0;i<matriz.length;i++) {
+			for (int j=0;j<matriz[i].length;j++) {
+				matriz[i][j] = sc.nextInt();
+			}
+		}
+		
+		System.out.println("Escreva um número X pertencente a matriz: ");
+		int x = sc.nextInt();
+		
+		//POSIÇÃO DE X
+		
+			for (int i=0;i<matriz.length;i++) {
+				for (int j=0;j<matriz[i].length;j++) {
+					if ( matriz[i][j] == x) {
+						System.out.println("Position " + i + ", " + j );
+						if (j>0) {
+							System.out.println("Left: " + matriz[i][j-1]);
+						}
+						if (i>0) {
+							System.out.println("Up: " + matriz[i-1][j]);
+						}
+						if (j<matriz[i].length-1) {
+							System.out.println("Right: " + matriz [i][j+1]); 
+						}
+						if (i<matriz.length-1) {
+							System.out.println("Down: " + matriz [i+1][j]);
+						}
+						
+						
+					}
+					
+				}
+			}
+
+				
+
+		sc.close();
+	}
+}
